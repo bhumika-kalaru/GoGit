e69d4321a883998d51c34d2f23eb75a131406db0
Bhumika Kalaru <bhumikakalaru@gmail.com>
2022-01-16 17:40:30 +0530

    Task 3 Completed

--- bhumika-kalaru.py
+++ bhumika-kalaru.py
@@ -1,2 +1,3 @@
 The 100
 The story is very intresting 
+Ques 5: It is very interesting and challenging. Learnt so many things in this workshop. Thanks you for all the efforts in conducting the workshop.
